37a03ab75af51efb0bcbed34b0cb03374a028160
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-20 21:45:49 +0100

    sort a doubly liked list using Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,63 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: doubly linked list
+ * Return: void
+ */
+
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *next, *prev;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	current = *list;
+	next = current->next;
+
+	while (next != NULL)
+	{
+		prev = current;
+		next = current->next;
+		if (current->n > next->n)
+		{
+			swap_nodes(current, next, list);
+			current = prev;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
+
+
+/**
+ * swap_nodes - swaps two nodes in a doubly linked list
+ * @current: current node
+ * @next: next node
+ * @list: doubly linked list
+ * Return: void
+ */
+
+void swap_nodes(listint_t *current, listint_t *next, listint_t **list)
+{
+	listint_t *prev, *next_next;
+
+	prev = current->prev;
+	next_next = next->next;
+
+	if (prev != NULL)
+		prev->next = next;
+	else
+		*list = next;
+
+	next->prev = prev;
+	next->next = current;
+	current->prev = next;
+	current->next = next_next;
+
+	if (next_next != NULL)
+		next_next->prev = current;
+}
--- sort.h
+++ sort.h
@@ -22,5 +22,7 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t *current, listint_t *next, listint_t **list);
 
 #endif /* SORT_H */
