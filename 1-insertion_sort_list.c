91440ef2e4628cb609664017cc5be361deba7e60
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-22 11:18:51 +0100

    fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *next, *prev;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	current = *list;
